28e9aa74f3c720b15fb823499cca888d3f878c64
salcrom <seya.28@gmail.com>
2024-12-04 12:14:47 +0100

    unicafe - paso listo

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,7 +6,29 @@ const App = () => {
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
 
-    return <div></div>;
+    return (
+        <div>
+            <h1>give feedback</h1>
+
+            <button onClick={() => setGood(good + 1)}>good</button>
+            <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+            <button onClick={() => setBad(bad + 1)}>bad</button>
+
+            <h2>statistics</h2>
+            <div>
+                <p>good {good}</p>
+                <p>neutral {neutral}</p>
+                <p>bad {bad}</p>
+                <p>all {good + neutral + bad}</p>
+                <p>
+                    average{" "}
+                    {(good * 1 + neutral * 0 + bad * -1) /
+                        (good + neutral + bad)}
+                </p>
+                <p>positive {(good / (good + neutral + bad)) * 100} %</p>
+            </div>
+        </div>
+    );
 };
 
 export default App;
